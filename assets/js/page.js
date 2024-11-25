cf4039d8afbc13158acc2c3d31e8ea9b97349008
gofry25 <tovstokorv@mail.ru>
2024-11-25 10:17:32 +0300

    taking map from mockapi

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,9 +1,17 @@
 // Получаем id карточки из URL
 const urlParams = new URLSearchParams(window.location.search);
 const cardId = urlParams.get('id');
-
 // URL вашего MockAPI
 const apiUrl = 'https://6735cb285995834c8a941c33.mockapi.io/card';
+let loader = document.getElementById("loader")
+let loader_bg = document.getElementById("loader-bg")
+
+window.onload = function() {
+    setTimeout(function() {
+    loader.style.display = 'none'
+    loader_bg.style.display = 'none'
+}, 2500)
+}
 
 // Функция для получения данных карточки по id
 async function fetchCardDetails(id) {
@@ -19,12 +27,14 @@ async function fetchCardDetails(id) {
 // Функция для отображения данных карточки
 function displayCardDetails(card) {
     const container = document.getElementById('card-details');
+    const map = document.getElementById('map')
     container.innerHTML = `
         <h1>${card.title}</h1>
         <img src="${card.image}" alt="${card.title}">
-        <img src="${card.image}" alt="${card.title}">
-        <img src="${card.image}" alt="${card.title}">
+        <img src="${card.image2}" alt="${card.title}">
+        <img src="${card.image3}" alt="${card.title}">
         <p>${card.longerDescription}</p>
+        ${card.map}
     `;
 }
 
