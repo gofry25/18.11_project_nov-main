7ec200e65a0654123a48e4f4a62e77ca2879e073
gofry25 <tovstokorv@bk.ru>
2024-12-09 14:08:03 +0300

    adapt

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,104 +8,121 @@ let loader_bg = document.getElementById("loader-bg")
 
 window.onload = function() {
     setTimeout(function() {
-    loader.style.display = 'none'
-    loader_bg.style.display = 'none'
-}, 500)
+        loader.style.display = 'none'
+        loader_bg.style.display = 'none'
+    }, 500)
 }
 
-async function fetchTotalCards() {
-    try {
-        const response = await fetch(apiUrl);
-        allCards = await response.json();
-        totalCards = allCards.length;
-        const totalPages = Math.ceil(totalCards / cardsPerPage);
-        return totalPages;
-    } catch (error) {
-        console.error('Error fetching total cards:', error);
+class Fetcher{
+
+    constructor(apiUrl, cardsPerPage, allCards, totalCards){
+        this.api = apiUrl
+        this.cardsPerPage = cardsPerPage
+        this.cardsArr = allCards
+        this.total = totalCards
+    }
+
+    async fetchTotalCards() {
+        try {
+            const response = await fetch(this.api);
+            this.allCards = await response.json();
+            this.total = this.allCards.length;
+            this.totalPages = Math.ceil(this.total / this.cardsPerPage);
+            return this.totalPages;
+        } catch (error) {
+            console.error('Error fetching total cards:', error);
+        }
+    }
+
+    async fetchCards(page) {
+        try {
+            const response = await fetch(`${this.api}?page=${page}&limit=${this.cardsPerPage}`);
+            const cards = await response.json();
+            display.displayCards(cards);
+            this.totalPages = await this.fetchTotalCards();
+            display.displayPagination(page, this.totalPages);
+        } catch (error) {
+            console.error('Error fetching cards:', error);
+        }
     }
 }
+let fetcher = new Fetcher(apiUrl, cardsPerPage, allCards, totalCards);
 
 
-async function fetchCards(page) {
-    try {
-        const response = await fetch(`${apiUrl}?page=${page}&limit=${cardsPerPage}`);
-        const cards = await response.json();
-        displayCards(cards);
-        const totalPages = await fetchTotalCards();
-        displayPagination(page, totalPages);
-    } catch (error) {
-        console.error('Error fetching cards:', error);
+class Display{
+    constructor(){
+
     }
-}
 
-
-function displayCards(cards) {
-    const container = document.getElementById('cards-container');
-    container.innerHTML = ''; 
-    cards.forEach(card => {
-        const cardElement = document.createElement('div');
-        cardElement.className = 'card';
-        cardElement.innerHTML = `
-            <h1>${card.title}</h1>
-            <p>${card.description}</p>
-            <img src="${card.image}" alt="${card.title}">
-        `;
-        cardElement.addEventListener('click', () => {
-            window.location.href = `page.html?id=${card.id}`;
+    displayCards(cards) {
+        const container = document.getElementById('cards-container');
+        container.innerHTML = ''; 
+        cards.forEach(card => {
+            const cardElement = document.createElement('div');
+            cardElement.className = 'card';
+            cardElement.innerHTML = `
+                <h1>${card.title}</h1>
+                <p>${card.description}</p>
+                <img src="${card.image}" alt="${card.title}">
+            `;
+            cardElement.addEventListener('click', () => {
+                window.location.href = `page.html?id=${card.id}`;
+            });
+            container.appendChild(cardElement);
         });
-        container.appendChild(cardElement);
-    });
 }
 
-
-function displayPagination(currentPage, totalPages) {
-    const paginationContainer = document.getElementById('pagination');
-    paginationContainer.innerHTML = ''; 
+    displayPagination(currentPage, totalPages) {
 
 
-    const prevButton = document.createElement('button');
-    prevButton.innerText = '←';
-    prevButton.disabled = currentPage === 1;
-    prevButton.addEventListener('click', () => {
-        if (currentPage > 1) {
-            fetchCards(currentPage - 1);
-        }
-    });
-    paginationContainer.appendChild(prevButton);
+        this.paginationContainer = document.getElementById('pagination');
+        this.paginationContainer.innerHTML = ''; 
 
-    const pageNum = document.createElement('p');
-    pageNum.textContent = currentPage
-    paginationContainer.appendChild(pageNum);
+        this.prevButton = document.createElement('button');
+        this.prevButton.innerText = '←';
+        this.prevButton.disabled = currentPage === 1;
+        this.prevButton.addEventListener('click', () => {
+            if (currentPage > 1) {
+                currentPage--;
+                fetcher.fetchCards(currentPage);
+            }
+        });
+        this.paginationContainer.appendChild(this.prevButton);
 
+        this.pageNum = document.createElement('p');
+        this.pageNum.textContent = currentPage;
+        this.paginationContainer.appendChild(this.pageNum);
 
-    const nextButton = document.createElement('button');
-    nextButton.innerText = '→';
-    nextButton.disabled = currentPage === totalPages;
-    nextButton.addEventListener('click', () => {
-        if (currentPage < totalPages) {
-            fetchCards(currentPage + 1);
-        }
-    });
-    paginationContainer.appendChild(nextButton);
+        this.nextButton = document.createElement('button');
+        this.nextButton.innerText = '→';
+        this.nextButton.disabled = currentPage === totalPages;
+        this.nextButton.addEventListener('click', () => {
+            if (currentPage < totalPages) {
+                currentPage++;
+                fetcher.fetchCards(currentPage);
+            }
+        });
+        this.paginationContainer.appendChild(this.nextButton);
 }
+}
+let display = new Display();
+
 
 
 function filterCards(searchTerm) {
-    const filteredCards = allCards.filter(card => {
+    const filteredCards = fetcher.allCards.filter(card => {
         return card.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
             card.description.toLowerCase().includes(searchTerm.toLowerCase());
     });
 
     const totalPages = Math.ceil(filteredCards.length / cardsPerPage);
-    displayCards(filteredCards.slice(0, cardsPerPage));
-    displayPagination(1, totalPages);
+    display.displayCards(filteredCards.slice(0, cardsPerPage));
+    display.displayPagination(1, totalPages);
 }
-
 
 document.getElementById('search').addEventListener('input', (event) => {
     const searchTerm = event.target.value;
     filterCards(searchTerm);
 });
 
-
-fetchCards(currentPage);
+fetcher.fetchCards(currentPage);
